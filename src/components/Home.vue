c18528798fbc3bec4b6324cc2fc5d1437521ba81
OriStranac <orio6165@gmail.com>
2024-02-01 22:38:28 +0100

    fixed pagination bug

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,52 +11,39 @@
       </div>
     </div>
     <div class="row mt-3">
-      <div v-for="book in currentBooks" :key="book.id" class="col-lg-4 mb-3">
+      <div
+        v-for="book in currentBooks"
+        :key="book.id"
+        class="col-lg-4 mb-3"
+      >
         <div class="card">
           <img
             :src="book.image"
             class="card-img-top"
             alt="Book Cover"
-            style="height: 200px; object-fit: cover"
+            style="height: 200px; object-fit: cover;"
           />
           <div class="card-body">
             <h5 class="card-title">{{ book.title }}</h5>
             <p class="card-text">{{ book.subtitle }}</p>
             <p class="card-text">Authors: {{ book.authors }}</p>
-            <a :href="book.url" target="_blank" class="btn btn-primary"
-              >More Info</a
-            >
+            <a :href="book.url" target="_blank" class="btn btn-primary">More Info</a>
           </div>
         </div>
       </div>
     </div>
-    <nav aria-label="Page navigation" v-if="myBooks && myBooks.length > 0">
+    <nav aria-label="Page navigation"  v-if="myBooks && myBooks.length > 0">
       <ul class="pagination">
         <li class="page-item" :class="{ disabled: currentPage === 1 }">
-          <a
-            class="page-link"
-            @click="changePage(currentPage - 1)"
-            href="#"
-            aria-label="Previous"
-          >
+          <a class="page-link" @click="changePage(currentPage - 1)" href="#" aria-label="Previous">
             <span aria-hidden="true">&laquo;</span>
           </a>
         </li>
-        <li
-          v-for="page in displayedPages"
-          :key="page"
-          class="page-item"
-          :class="{ active: currentPage === page }"
-        >
+        <li v-for="page in displayedPages" :key="page" class="page-item" :class="{ active: currentPage === page }">
           <a class="page-link" @click="changePage(page)" href="#">{{ page }}</a>
         </li>
         <li class="page-item" :class="{ disabled: currentPage === totalPages }">
-          <a
-            class="page-link"
-            @click="changePage(currentPage + 1)"
-            href="#"
-            aria-label="Next"
-          >
+          <a class="page-link" @click="changePage(currentPage + 1)" href="#" aria-label="Next">
             <span aria-hidden="true">&raquo;</span>
           </a>
         </li>
@@ -86,23 +73,26 @@ export default {
     },
     displayedPages() {
       const maxDisplayedPages = 5;
-      const startPage = Math.max(
-        this.currentPage - Math.floor(maxDisplayedPages / 2),
-        1
-      );
-      const endPage = Math.min(
-        startPage + maxDisplayedPages - 1,
-        this.totalPages
-      );
-      return Array.from(
-        { length: endPage - startPage + 1 },
-        (_, i) => startPage + i
-      );
+      const startPage = Math.max(this.currentPage - Math.floor(maxDisplayedPages / 2), 1);
+      const endPage = Math.min(startPage + maxDisplayedPages - 1, this.totalPages);
+      return Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i);
     },
   },
   methods: {
-    ...mapActions(["fetchDataFromAPI", "changePage"]),
+    ...mapActions(["fetchDataFromAPI"]),
     ...mapMutations(["SET_TEXT_INPUT"]),
+    async changePage(page) {
+      if (page >= 1 && page <= this.totalPages) {
+        this.$store.commit("SET_CURRENT_PAGE", page);
+
+        // Provera da li je stranica već dohvaćena
+        if (!this.$store.state.pagesData[page]) {
+          // Ako nije, dohvat podataka sa API-ja
+          const data = await this.fetchDataFromAPI(this.textInput);
+          this.$store.commit("SET_PAGES_DATA", { page, data });
+        }
+      }
+    },
   },
   watch: {
     textInput(newText) {
