11aa60512e8afea515a0ca298498f087f7a0f9ac
OriStranac <orio6165@gmail.com>
2024-02-06 23:30:18 +0100

    write code for footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="text-center fixed-bottom" style="height: 52px; line-height: 52px;">
+    © 2023 BookFinder. All rights reserved.
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+</style>
