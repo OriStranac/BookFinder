a4edda79a1817f77515cd6310f6ea3a5149e4ca3
OriStranac <orio6165@gmail.com>
2024-02-06 23:31:35 +0100

    add footer to app component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <div>
     <Header></Header>
-    <router-view></router-view>
+    <div class="main-content bg-light">
+      <router-view></router-view>
+    </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue"
 
 export default {
   name: "App",
   components: {
     Header,
+    Footer,
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.main-content {
+  min-height: calc(100vh - 52px * 2);
+  padding-top: 52px; 
+  padding-bottom: 52px;
+  width: 100%;
+}
+</style>
